ffc6fdea43884aca1c43abb4931372acfa4f5bc2
iot-lsy <suwonbank@gmail.com>
2021-07-16 01:42:32 +0900

    전체기능구현

--- probono-arduino.ino
+++ probono-arduino.ino
@@ -1,141 +1,103 @@
-#include "LedControl.h"
 #include <SoftwareSerial.h>
 
-#define RX 2
-#define TX 3
+#define voice_recogn_RX 2
+#define voice_recogn_TX 3
+#define button_IN 12
+#define pir_IN 10
+#define bluetooth_RX 6
+#define bluetooth_TX 7
 
-SoftwareSerial mySerial(RX, TX);
+#define voice_emergency 111 // 음성 응급호출 111 전송
+#define button_emergency 112 // 버튼 응급호출 112 전송
+#define person_in 121 // pir센서 들어오는 경우 121 전송
+#define person_out 122 // pir센서 나가는 경우 122 전송
 
+SoftwareSerial mySerial_voice(voice_recogn_RX, voice_recogn_TX);
+SoftwareSerial BT(bluetooth_RX, bluetooth_TX);
 
-// Din 핀을 12번, ClK핀을 11번 CS핀을 10번에 연결
-// 매트릭스는 1개를 사용 선언
-LedControl lc = LedControl(12,11,10,1);
+int voice_input_hex = 0;
+byte buffer[128];
 
-int voice_recogn = 0;
-
-byte data[ 5 ][ 8 ] =
-{
-// 무표정 이미지
-{
-B11111111,
-B10000001,
-B10100101,
-B10000001,
-B10000001,
-B10111101,
-B10000001,
-B11111111
-},
-// 웃는 표정 이미지
-{
-B11111111,
-B10000001,
-B10100101,
-B10000001,
-B10100101,
-B10111101,
-B10000001,
-B11111111
-},
-
-// 십자가 모양
-{
-B00011000,
-B00011000,
-B00011000,
-B11111111,
-B11111111,
-B00011000,
-B00011000,
-B00011000
-},
-// 하트 모양
-{
-B00000000,
-B01100110,
-B11111111,
-B11111111,
-B11111111,
-B01111110,
-B00111100,
-B00011000
-},
-// 동그란 웃는모양
-{
-B00111100,
-B01000010,
-B10100101,
-B10000001,
-B10100101,
-B10011001,
-B01000010,
-B00111100
-}
-};
+void start_voice_recogn();
+void voice_recogn();
+void button_pushed();
+void pir_recogn();
+void start_bluetooth();
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  pinMode(button_IN, INPUT);
+  pinMode(pir_IN, INPUT);
   Serial.begin(9600);
-  mySerial.begin(9600);
+  BT.begin(9600);
+  mySerial_voice.begin(9600);
+  set_voice_recogn();
+  
+}
 
-  lc.shutdown(0, false);
-  lc.setIntensity(0, 1);
-  lc.clearDisplay(0);
+void loop() {
 
+  if(BT.available()){
+  voice_recogn();
+  button_pushed();
+  pir_recogn();
+  }
+  
+  delay(1000);
+}
+
+void set_voice_recogn(){ // 음성인식 모듈 활성화
   Serial.println("wait settings are in progress");
   delay(1000);
-  mySerial.write(0xAA); // compact mode
-  mySerial.write(0x37);
+  mySerial_voice.write(0xAA); // compact mode
+  mySerial_voice.write(0x37);
   delay(1000);
-  mySerial.write(0xAA);
-  mySerial.write(0x21);
+  mySerial_voice.write(0xAA);
+  mySerial_voice.write(0x21);
 
   Serial.println("The settings are complete");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-  while(mySerial.available()){
+void voice_recogn(){ // 음성인식 
+  while(mySerial_voice.available()){
     Serial.println("Voice input");
-    voice_recogn = mySerial.read();
+    voice_input_hex = mySerial_voice.read();
     Serial.print("voice_recogn:");
-    Serial.println(voice_recogn);
+    Serial.println(voice_input_hex);
     
-    switch(voice_recogn){
+    switch(voice_input_hex){
       case 0x11:
-        for(int i=0; i<8; i++){
-            lc.setRow(0, i, data[0][i]);
-        }
-        Serial.println("무표정 이미지 출력");
-        break;
-
       case 0x12:
-        for(int i=0; i<8; i++){
-          lc.setRow(0, i, data[1][i]);
-        }
-        Serial.println("웃는 표정 이미지 출력");
-        break;
-
       case 0x13:
-        for(int i=0; i<8; i++){
-          lc.setRow(0, i, data[2][i]);
-        }
-        Serial.println("십자가 모양 이미지 출력");
-        break;
-
       case 0x14:
-        for(int i = 0 ; i < 8 ; i ++){
-          lc.setRow( 0, i, data[3][i]); // 하트 모양 이미지 출력
-        }
-        Serial.println("하트 모양 이미지 출력");
-        break ;
       case 0x15 :
-        for(int j=0; j<8; j++){
-          lc.setRow( 0 ,j,data[ 4 ][j]); // 동그란 웃는 표정 이미지출력
-        }
-        Serial.println("동글 웃는 이미지 출력");
+        Serial.println("음성 응급호출");
+        BT.write(buffer, voice_emergency);
         break;
     }
+  }
+}
+
+void button_pushed(){ // 응급호출 버튼
+  int value = digitalRead(button_IN);
+
+  if(value == HIGH){
+    Serial.println("Emergency Button Pushed");
+    BT.write(buffer, button_emergency);
+  }
+}
+
+void pir_recogn(){ // (!!!! 수정 필요<계속 감지되는 경우에 대한 예외처리 해야함> !!!!)
+  int value = digitalRead(pir_IN);
+  int count = 0;
+
+  if(value == HIGH && count==0){ // 들어오는 경우
+    Serial.println("Movement recognition");
+    BT.write(buffer, person_in);
+    count++;
+  }else if(value == HIGH && count!=0){ // 나가는 경우
+    Serial.println("Movement recognition");
+    BT.write(buffer, person_out);
+    count = 0;
   }
 }
